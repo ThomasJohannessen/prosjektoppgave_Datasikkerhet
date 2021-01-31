dc143d754960d5766670e536317c1a1e8441d92b
Michal <mkowalski1999@gmail.com>
2021-01-31 21:28:08 +0100

    Oppdaterte til nyeste api filer for fetch query og database

--- api/fetch.php
+++ api/fetch.php
@@ -1,16 +1,18 @@
 <?php 
 
-$json_file = file_get_contents("http://localhost/nett/query.php");
+$json_file = file_get_contents("http://localhost/nett/api-final/api_hentSvar.php");
 
 echo $json_file;
 $json_decoded = json_decode($json_file, true);
 echo "<br>";
 echo print_r($json_decoded);
 echo "<br>";
-echo "decoded ";
-echo $json_decoded;
 echo "<br>";
-/*foreach ($json_decoded as $k => $v) {
-    echo $k, ' : ', $v;
- }*/
+foreach (array_values($json_decoded) as $array) {
+    foreach($array as $key=>$value) {
+        echo "<br>";
+        echo "$key : $value";
+    }
+    
+ }
 ?>
