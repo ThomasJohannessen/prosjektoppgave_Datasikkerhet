02bf14b48122b462be634bf3011eb23dd4c31aa3
MapNex <54624778+MapNex@users.noreply.github.com>
2021-02-02 19:05:23 +0100

    another change

--- database.php
+++ database.php
@@ -1,30 +1,9 @@
 <?php
-$mysqli = new mysqli("localhost", "softsec", "password", "datasikkerhet_prosjekt");
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-else
-{
-    echo "Connected to DB";
-}
+$conn = new mysqli("localhost", "softsec", "password", "datasikkerhet_prosjekt");
 
-$query = "SELECT * FROM brukere";
-
-if ($result = $mysqli->query($query)) {
-
-    /* fetch object array */
-    while ($row = $result->fetch_row()) {
-        printf ("%s (%s)\n", $row[0]);
-    }
-
-    /* free result set */
-    $result->close();
+if($conn->connect_error) {
+    die($conn->connect_errno. ": ".$conn->connect_error);
 }
 
 
-/* close connection */
-$mysqli->close();
-?>
