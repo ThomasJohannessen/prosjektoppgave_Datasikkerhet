4692fcec4f1f01d91942e4c22ca9c206b7ac94f0
MapNex <54624778+MapNex@users.noreply.github.com>
2021-02-02 18:55:00 +0100

    enda en test

--- database.php
+++ database.php
@@ -1,20 +1,25 @@
 <?php
+$mysqli = new mysqli("localhost", "root", "", "datasikkerhet_prosjekt");
 
-$db = "localhost";
-$username = "root";
-$password = "";
-$dbname = "datasikkerhet_prosjekt";
-
-$conn = mysqli_connect($db, $username, $password, $dbname);
-
-if($conn->connect_error) {
-    die($conn->connect_errno. ": ".$conn->connect_error);
-}
-else{
-    echo"connected to server";
-
-
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
 
+$query = "SELECT * FROM brukere";
 
+if ($result = $mysqli->query($query)) {
+
+    /* fetch object array */
+    while ($row = $result->fetch_row()) {
+        printf ("%s (%s)\n", $row[0]);
+    }
+
+    /* free result set */
+    $result->close();
+}
+
+/* close connection */
+$mysqli->close();
 ?>
