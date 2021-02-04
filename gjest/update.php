bc4c463f7096eb2aa4ac39b1db7c90f20313eb67
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-02-04 16:15:26 +0100

    mer fiksing

--- gjest/update.php
+++ gjest/update.php
@@ -3,15 +3,7 @@
 include "../database.php";
 include "../foreleser/index.php";
 
-$conn = mysqli_connect($db, $username, $password, $dbname);
-
-if (!$conn) {
-    echo "Not connected to server";
-}
-
-if(!mysqli_select_db($conn, $dbname)) {
-    echo "Database not selected";
-}
+global $conn;
 
 $kommentar = $_POST['comment'];
 $sporsmalID = $_POST['messageID'];
