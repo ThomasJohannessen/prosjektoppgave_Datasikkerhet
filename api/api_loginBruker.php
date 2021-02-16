d41d90776986426e348df06ac3b731793ec74ff9
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-02-16 20:26:27 +0100

    Update api_loginBruker.php

--- api/api_loginBruker.php
+++ api/api_loginBruker.php
@@ -5,11 +5,11 @@ $db = $db_conn->get_Connection() or die();
 
 $epost = $_GET['epost'];
 $passord = $_GET['passord'];
-$passord_hashed = password_hash($passord, PASSWORD_DEFAULT);
-echo $passord;
-echo "\n";
-echo $epost;
-echo "\n";
+//$passord_hashed = password_hash($passord, PASSWORD_DEFAULT);
+//echo $passord;
+//echo "\n";
+//echo $epost;
+//echo "\n";
 
 
 
@@ -21,14 +21,30 @@ echo "\n";
 
 //$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '".$epost."' AND Passord = '".password_hash($password, PASSWORD_DEFAULT)."';";
 //$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '.$epost.' AND Passord = '.$passord_hashed.'";
-$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '".$epost."' AND Passord = '".$passord_hashed."';";
+//$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '".$epost."' AND Passord = '".$passord_hashed."';";
+
+//$password_query = "SELECT Passord FROM `brukere` WHERE Epost = '".$epost."';";
+//$password_result = $db->query($password_query);
+//
+//$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '".$epost."';";
+$query = "SELECT BrukerID, Passord FROM `brukere` WHERE Epost = '".$epost."';";
 $result = $db->query($query);
 $db_conn->close_Connection();
 
+//if($password_result->num_rows >= 1) {
+//    $passord_row = $password_result->fetch_assoc();
+//    $password_hash = $passord_row["Passord"];
+//}
 if($result->num_rows >= 1) {
     $row = $result->fetch_assoc();
-    echo $row["BrukerID"];
-    //returnerer brukerid .. Mulig å legeg til flere ting fra db her ved behov
+    $password_hash = $row["Passord"];
+    if (password_verify($passord, $password_hash)) {
+        echo 'Password is valid!\n';
+        echo $row["BrukerID"];
+    } else {
+        echo 'Invalid password.';
+    }
+    
 }
 else {
     echo 0;
