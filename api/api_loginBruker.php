fb746a91d9d836d041a69d23e7fcdd34ea322958
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-02-16 21:10:44 +0100

    Update api_loginBruker.php

--- api/api_loginBruker.php
+++ api/api_loginBruker.php
@@ -16,20 +16,16 @@ $db_conn->close_Connection();
 
 $passord_row = $password_result->fetch_assoc();
 $password_hash = $passord_row["Passord"];
-
-echo $passord;
-echo $password_hash;
     
-if((password_verify($passord, $password_hash))) {
-    echo "Passord stemmer";
-    if($result->num_rows == 1){
+if(($result->num_rows == 1)&&(password_verify($passord, $password_hash))) {
+    
     $json_array = array();
     $row = $result->fetch_assoc();
     array_push($json_array, $row);
     $json_array = json_encode($json_array);
     echo $json_array;
     echo "Fungerte. Riktig passord og 1 svar";   
-}}
+}
 else {
     echo 0;
 }
