179d44d77fde807c8d5d6b5746a950c45891275f
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-02-16 18:24:30 +0100

    Update api_loginBruker.php

--- api/api_loginBruker.php
+++ api/api_loginBruker.php
@@ -6,14 +6,16 @@ $db = $db_conn->get_Connection() or die();
 
 $epost = /*PLACEHOLDER*/ "Pia@hiof.no";//variabel for epost bruker har tastet inn i app
 $passord = /*PLACEHOLDER*/ "Nodeland1";//varaibel for passord bruker har tastet inn i app
+$passord_hashed = password_hash(passord, PASSWORD_DEFAULT);
+echo $passord_hashed;
 //$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '".$epost."' AND Passord = '".password_hash($password, PASSWORD_DEFAULT)."';";
-$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '.$epost.' AND Passord = '.password_hash($password, PASSWORD_DEFAULT).'";
+$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '.$epost.' AND Passord = '.$passord_hashed.'";
 $result = $db->query($query);
 $db_conn->close_Connection();
 
 if($result->num_rows == 0) {
     //innlogging finnes ikke 
-    echo 0;
+    echo "DET FINNES INGEN BRUKER DER EPOST ER:'.$epost.' AND Passord = '.$passord_hashed.'";
 } 
 else {
     $row = $result->fetch_assoc();
