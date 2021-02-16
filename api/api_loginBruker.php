b5945c0924358ceacf2f42bc4a1c628fa1e06862
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-02-16 19:54:45 +0100

    Update api_loginBruker.php

--- api/api_loginBruker.php
+++ api/api_loginBruker.php
@@ -1,5 +1,4 @@
 <?php
-
 include "../database.php";
 $db_conn = new Database();
 $db = $db_conn->get_Connection() or die();
@@ -11,23 +10,40 @@ $passord_hashed = password_hash($passord, PASSWORD_DEFAULT);
 echo $epost;
 echo "\n";
 
+
+
+
+//$epost = /*PLACEHOLDER*/ "Pia@hiof.no";//variabel for epost bruker har tastet inn i app
+//$passord = /*PLACEHOLDER*/ "Nodeland1";//varaibel for passord bruker har tastet inn i app
+//$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '".$epost."' AND Passord = '".password_hash($password, PASSWORD_DEFAULT)."';";
+
+
 //$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '".$epost."' AND Passord = '".password_hash($password, PASSWORD_DEFAULT)."';";
 //$query = "SELECT BrukerID FROM `brukere` WHERE Epost = '.$epost.' AND Passord = '.$passord_hashed.'";
 $query = "SELECT BrukerID FROM `brukere` WHERE Epost = '.$epost.'";
 $result = $db->query($query);
 $db_conn->close_Connection();
 
-$json_array = array();
-if($result->num_rows > 0) {
-    echo "Brukeren finnes";
-    while($row = $result->fetch_assoc()) {
-        array_push($json_array, $row);
-    }
+if($result->num_rows >= 1) {
+    $row = $result->fetch_assoc();
+    echo $row["BrukerID"];
+    //returnerer brukerid .. Mulig å legeg til flere ting fra db her ved behov
 }
 else {
     echo 0;
 }
 
-$json_array = json_encode($json_array);
-echo $json_array;
+//$json_array = array();
+//if($result->num_rows > 0) {
+//    echo "Brukeren finnes";
+//    while($row = $result->fetch_assoc()) {
+//        array_push($json_array, $row);
+//    }
+//}
+//else {
+//    echo 0;
+//}
+//$json_array = json_encode($json_array);
+//echo $json_array;
+
 ?>
