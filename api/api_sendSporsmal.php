8a3dfe953eebd247c808d2ecdc25dc89b117dddd
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-02-18 17:08:06 +0100

    Fikset api til å ta imot parametere

--- api/api_sendSporsmal.php
+++ api/api_sendSporsmal.php
@@ -3,15 +3,14 @@ include "../database.php";
 $db_conn = new Database();
 $db = $db_conn->get_Connection() or die();
 
-$sporsmal = /*PLACEHOLDER*/ "Dette er et spørsmål fra API";//for tekst i spørsmålet
-$avsender = /*PLACEHOLDER*/ 11;//for brukerid på avsender
-$emnekode = /*PLACEHOLDER*/ "ITF887";//for fag spørsmåøet sendes til
+$sporsmal = $_GET['sporsmal'];
+$avsender = $_GET['brukerid'];
+$emnekode = $_GET['emnekode'];
 
 $query = 'INSERT INTO meldinger(avsenderID, emnekode, melding)
     VALUES('.$avsender.', "'.$emnekode.'", "'.$sporsmal.'")';
 
 $db->query($query);
 $db_conn->close_Connection();
-header("Location: http://158.39.188.201/steg1/prosjektoppgave_Datasikkerhet/index.php"); //placeholder for videresending etter sendt melding
 die();
 ?>
