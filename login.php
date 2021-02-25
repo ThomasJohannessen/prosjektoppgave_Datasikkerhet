ffa984ead136b937fcf402716f7e43aedff7a1f2
Michal <mkowalski1999@gmail.com>
2021-02-25 16:46:19 +0100

    god pls work

--- login.php
+++ login.php
@@ -11,7 +11,9 @@
 <body>
 <?php
 
-    
+require __DIR__.'/../../../composershit/vendor/autoload.php';
+use Monolog\Logger;
+use Monolog\Handler\GelfHandler;
 
     session_start();
     include("database.php");
@@ -101,10 +103,9 @@
                 $_SESSION["study_path"] = $mailTaken["Studieretning"];
 
                         
-                $logg = new AppLogger("innlogging");
-                $logger = $logg->getLogger();
-                $logger->info("Test");  
-                $logger->error("Testerror");
+                $logger = new Logger('sikkerhet');
+                $logger->pushHandler(
+                    new GelfHandler(new Gelf\Publisher(new Gelf\Transport\UdpTransport("127.0.0.1", 12201))));
                 $logger->debug("testdebug");
 
                 if ($_SESSION['user_type'] == 3){
