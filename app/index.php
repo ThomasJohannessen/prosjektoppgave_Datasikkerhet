070d81b08e28c4db095a8c1988f63b897af38728
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-04-08 15:21:19 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,9 +1,16 @@
-<?php
-    header('Content-Type: application/download');
-    header('Content-Disposition: attachment; filename="app.apk"');
-    header("Content-Length: " . filesize("app.apk"));
-
-    $fp = fopen("app.apk", "r");
-    fpassthru($fp);
-    fclose($fp);
-?>
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <title>APP dokumentasjon</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <main>
+            <h1> APP </h1>
+            <p>Dette er nedlastningssiden for vår Android App. Trykk på "Last ned"-knappen for å laste ned Appen. Husk at du må gjøre dette på din A$
+          <form action="https://158.39.188.201/steg2/prosjektoppgave_Datasikkerhet/app/download.php">
+                 <input type="submit" value="Last ned!"/>
+        </form>
+        </main>
+    </body>
+</html>
