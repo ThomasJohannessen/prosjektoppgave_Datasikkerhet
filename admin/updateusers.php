d75fe7d7e629af92f8dc29d0776c936288b1a991
MapNex <54624778+MapNex@users.noreply.github.com>
2021-04-15 19:13:43 +0200

    updated prepared statements

--- admin/updateusers.php
+++ admin/updateusers.php
@@ -41,25 +41,21 @@
 					$db = new Database();
 					$conn = $db->get_Connection("admin");
 
-					$stmt = $conn->prepare("CALL GetNameAndEmailOfAllStudentsAndLecturers(?)");
-					$stmt->bind_param("s", $email);
-					$stmt->execute();
-					$result = $stmt->get_result();
-					$user = $result->fetch_assoc();
-					
+                    $sql = "CALL GetNameAndEmailOfAllStudentsAndLecturers()";
 
+                    $records = mysqli_query($conn, $sql);
 
-					while($user != null)
-					{
-			?>
-  			<tr>
-				<form method="post">
-    					<td><input type="text" value="<?php echo $user["Navn"]; ?>" name="name"/></td>
-    					<td><input type="text" value="<?php echo $user["Epost"]; ?>" name="updatedEmail"/></td>
-    					<input type="hidden" value="<?php echo $user["Epost"]; ?>" name="originalEmail"/>
-	    		    		<td><input type="submit" name="update" value="Update" /></td>
-	    		    		<td><input type="submit" name="delete" value="Delete" /></td>
-	    		    	</form>
+                    while($data = mysqli_fetch_array($records))
+                    {
+                        ?>
+                        <tr>
+                            <form method="post">
+                                <td><input type="text" value="<?php echo $data["Navn"]; ?>" name="name"/></td>
+                                <td><input type="text" value="<?php echo $data["Epost"]; ?>" name="updatedEmail"/></td>
+                                <input type="hidden" value="<?php echo $data["Epost"]; ?>" name="originalEmail"/>
+                                <td><input type="submit" name="update" value="Update" /></td>
+                                <td><input type="submit" name="delete" value="Delete" /></td>
+                            </form>
   			</tr>	
 			<?php
 				if(isset($_POST['update'])) {
