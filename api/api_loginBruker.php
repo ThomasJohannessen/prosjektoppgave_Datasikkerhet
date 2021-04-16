5ad86387ca9185e5f581ff96f2f3cbc132260842
MapNex <54624778+MapNex@users.noreply.github.com>
2021-04-16 15:35:23 +0200

    updated prepared statements

--- api/api_loginBruker.php
+++ api/api_loginBruker.php
@@ -40,8 +40,6 @@ $logger = $logg->getLogger();
 
 $passord_row = $result2->fetch_assoc();
 $password_hash = $passord_row["Passord"];
-echo password_verify($passord, $password_hash);
-echo $password_hash;
 if(($result1->num_rows == 1)&&(password_verify($passord, $password_hash))) {
 
     $logger->info("User logged in", ["eMail" => $epost, "password" => $password_hash]);
